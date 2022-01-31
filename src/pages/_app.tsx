8c7c9d9640daa3c40c9fd6b274665e73d1b4c90a
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-31 14:34:47 -0300

    feat: add styles app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import type { AppProps } from 'next/app'
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
+import styles from '../styles/App.module.scss'
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
 }
 
 export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&family=Poppins:wght@400;500;600&display=swap"
           rel="stylesheet"
         ></link>
+
+        <link rel="icon" href="favicon.png" type="image/png" />
       </Head>
 
       <body>
