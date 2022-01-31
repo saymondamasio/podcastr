47fff5f8bb1a592fd76e78d967d5f32ea6c011df
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-31 14:34:11 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from 'next'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>Ola</div>
+  return (
+    <div className={styles.container}>
+      <h1>ola</h1>
+    </div>
+  )
 }
 
 export default Home
